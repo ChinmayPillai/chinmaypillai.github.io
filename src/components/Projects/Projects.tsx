c65673e6d57b933ed9f56e4b11a8e0586c46820e
Chinmay Pillai <chinmay.pillai@gmail.com>
2023-12-10 20:07:19 +0530

    Add Project descriptions

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,47 +4,53 @@ import ProjectCard, { Project } from "../Utilities/ProjectCard";
 const ProjectList: Project[] = [
   {
     title: "Speech Detection ML Model",
-    //des: "Upcoming Summer SDE Intern for SRI, Banglore",
+    des: "Neural-Network Model that detects speech segments of audio using MFCC features",
     date: "Oct-Nov 2023",
   },
   {
-    title: "Music Stroke Detection & Clustering ML Model",
-    //des: "Worked on IoT, MERN Stack, Computer Networks",
+    title: "Stroke Detection & Clustering ML Model",
+    des: "Neural-Network + K-Means Clustering Model that detects & \
+          clusters different musical instrument strokes",
     date: "Oct-Nov 2023",
   },
   {
     title: "MERN E-Commerce Web Application",
-    //des: "Worked on Neural Networks using PyTorch",
+    des: "Full-Stack E-Commerce Web Application using MERN stack with User Authentication and Encryption",
     date: "May-July 2023",
   },
   {
-    title: "Multi-Particle Force Simulator",
-    //des: "Worked on C++ Drivers, N-Body Simulator, Video Processing",
+    title: "Multi-Particle Gravity Simulator",
+    des: "Gravity/Coulomb Force Simulator with any number of particles. Solar System Simulator",
     date: "Oct-Nov 2022",
   },
   {
+    title: "React Typescript Portfolio Website ",
+    des: "This website which was made using React, Material UI and Bootstrap using Typescript",
+    date: "June 2023 - Present",
+  },
+  {
     title: "Singing Detection ML Model",
-    //des: "Worked on C++ Drivers, N-Body Simulator, Video Processing",
+    des: "Neural-Network Model that detects segments in audio with singing voices using MFCC features",
     date: "Oct-Nov 2023",
   },
   {
-    title: "Raytracing",
-    //des: "Worked on C++ Drivers, N-Body Simulator, Video Processing",
+    title: "Raytracing & Procedural Generation",
+    des: "Implement Raytracing and Procedural Generation for developing Computer Graphics using OOPS",
     date: "May-July 2023",
   },
   {
     title: "Live Focussed Frame Finder",
-    //des: "Worked on C++ Drivers, N-Body Simulator, Video Processing",
+    des: "Classify frames of a live video feed int focussed or unfocussed. Made for microscope feed",
     date: "Dec 2022",
   },
   {
     title: "Blockchain Voting Smart Contract",
-    //des: "Worked on C++ Drivers, N-Body Simulator, Video Processing",
+    des: "Voting Smart Contract on Solana Blockchain using Solidity Blockcahin. IIT Kanpur Workshop",
     date: "Mar 2023",
   },
   {
     title: "BASH Image Processing",
-    //des: "Worked on C++ Drivers, N-Body Simulator, Video Processing",
+    des: "BASH scripts to process images from a microscope using imagemagik package",
     date: "Dec 2022",
   },
 ]
