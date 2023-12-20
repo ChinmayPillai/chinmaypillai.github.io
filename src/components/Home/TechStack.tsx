6204ac6b93f3914cf784ddc45b6cf94c6e94cfb4
Chinmay Pillai <chinmay.pillai@gmail.com>
2023-12-20 16:59:46 +0530

    Update Tech Stack

--- src/components/Home/TechStack.tsx
+++ src/components/Home/TechStack.tsx
@@ -16,7 +16,9 @@ export default function TechStack(){
 
             <br/>
 
-            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>Programming Languages:</Typography>
+            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>
+                Programming Languages:
+            </Typography>
             <p style={{textAlign:"left"}}>  
                 <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width={imgSize} height={imgSize}/> </a>
                 <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="c" width={imgSize} height={imgSize}/> </a>
@@ -29,7 +31,9 @@ export default function TechStack(){
 
             <br/>
 
-            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>AI/ML:</Typography>
+            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>
+                AI/ML:
+            </Typography>
             <p style={{textAlign:"left"}}>  
                 <a href="https://www.tensorflow.org" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg" alt="tensorflow" width={imgSize} height={imgSize}/> </a>
                 <a href="https://pytorch.org/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://www.vectorlogo.zone/logos/pytorch/pytorch-icon.svg" alt="pytorch" width={imgSize} height={imgSize}/> </a>
@@ -41,7 +45,9 @@ export default function TechStack(){
 
             <br/>
             
-            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>Backend Development:</Typography>
+            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>
+                Backend Development:
+            </Typography>
             <p style={{textAlign:"left"}}> 
                 <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width={imgSize} height={imgSize}/> </a>
                 <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width={imgSize} height={imgSize}/> </a>
@@ -52,7 +58,9 @@ export default function TechStack(){
             <br/>
             
 
-            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>Frontend Development:</Typography>
+            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>
+                Frontend Development:
+            </Typography>
             <p style={{textAlign:"left"}}>  
                 <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width={imgSize} height={imgSize}/> </a>
                 <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width={imgSize} height={imgSize}/> </a>
@@ -62,18 +70,22 @@ export default function TechStack(){
 
             <br/>
             
-            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>Database:</Typography>
+            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>
+                Database:
+            </Typography>
             <p style={{textAlign:"left"}}> 
                 <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width={imgSize} height={imgSize}/> </a>
                 <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width={imgSize} height={imgSize}/> </a>
                 <a href="https://www.postgresql.org" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width={imgSize} height={imgSize}/> </a>
-            
+                <a href="https://redis.io" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redis/redis-original-wordmark.svg" alt="redis" width={imgSize} height={imgSize}/> </a>
             </p>
 
 
             <br/>
             
-            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>DevOps:</Typography>
+            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>
+                DevOps:
+            </Typography>
             <p style={{textAlign:"left"}}>  
                 <a href="https://www.docker.com/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="docker" width={imgSize} height={imgSize}/> </a>
                 <a href="https://aws.amazon.com" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" alt="aws" width={imgSize} height={imgSize}/> </a>
@@ -82,14 +94,18 @@ export default function TechStack(){
 
             <br/>
             
-            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>OS:</Typography>
+            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>
+                OS:
+            </Typography>
             <p style={{textAlign:"left"}}>  
                 <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width={imgSize} height={imgSize}/> </a> 
             </p>
 
             <br/>
             
-            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>CI/CD:</Typography>
+            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>
+                CI/CD:
+            </Typography>
             <p style={{textAlign:"left"}}>  
                 <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width={imgSize} height={imgSize}/> </a>
             </p>
@@ -97,16 +113,21 @@ export default function TechStack(){
 
             <br/>
             
-            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>Frameworks:</Typography>
+            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>
+                Frameworks:
+            </Typography>
             <p style={{textAlign:"left"}}>  
                 <a href="https://dotnet.microsoft.com/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-original-wordmark.svg" alt="dotnet" width={imgSize} height={imgSize}/> </a>
             </p>
 
             <br/>
             
-            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>Other Software:</Typography>
+            <Typography variant="h4" color={textColour} style={{textAlign:"left"}}>
+                Other Software:
+            </Typography>
             <p style={{textAlign:"left"}}> 
                 <a href="https://www.mathworks.com/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png" alt="matlab" width={imgSize} height={imgSize}/> </a>
+                <a href="https://postman.com" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width={imgSize} height={imgSize}/> </a>
                 <a href="https://www.arduino.cc/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://cdn.worldvectorlogo.com/logos/arduino-1.svg" alt="arduino" width={imgSize} height={imgSize}/> </a> 
                 <a href="https://unity.com/" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://www.vectorlogo.zone/logos/unity3d/unity3d-icon.svg" alt="unity" width={imgSize} height={imgSize}/> </a>
                 <a href="https://www.photoshop.com/en" target="_blank" rel="noreferrer"> <img style={imgBg} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg" alt="photoshop" width={imgSize} height={imgSize}/> </a>
