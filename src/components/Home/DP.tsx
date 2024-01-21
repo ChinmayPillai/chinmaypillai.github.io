a77dd41dbd15a8f4eec7aad55f23bd1e71b24d41
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-01-21 16:59:55 +0530

    Add Typewritter animation to home page

--- src/components/Home/DP.tsx
+++ src/components/Home/DP.tsx
@@ -4,7 +4,7 @@ function DP() {
       src="/DP.jpg"
       className="rounded float-end mt-2 border border-1 border-dark-subtle centre"
       alt="Couldn't Load Image :("
-      style={{ width: "60%", height: "auto" }}
+      style={{ width: "100%", height: "auto" }}
     ></img>
   );
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,51 +1,78 @@
 import DP from "./DP";
 import TechStack from "./TechStack";
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography, createTheme, ThemeProvider, responsiveFontSizes } from "@mui/material";
 import { headingStyle, textColour } from "../Utilities/colors";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 function Home() {
 
+    const [myPassions] = useTypewriter({
+        words: ["Full-Stack Web Dev.", "Machine Learning.", "Blockchain."],
+        loop: true,
+    })
+
     return (
         <Container sx={{ mb: 10}}>
-            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-                {/* <Grid item xs={12} >
-                    
-                </Grid> */}
-                <Grid item xs={8}>  
-                    <div className="mt-2 mb-2">
-                        <Typography variant="h4" align="left" color={textColour}>
-                            Hi, I'm 
-                        </Typography>
-                        <Typography variant="h1" align="left" color={headingStyle}>
-                            Chinmay Pillai
-                        </Typography>
-                        <Typography variant="h2" align="left" color={headingStyle}>
-                            I'm a Developer
-                        </Typography>
-                    </div>
-
-                    <Typography variant="h6" align="left" color={textColour} >
-                        I am a curious developer studying in IIT Kanpur
-                        with <span style={{ fontWeight: 'bold' }}>1.5 years</span> of 
-                        internship experience in software. I have worked 
-                        on <span style={{ fontWeight: 'bold' }}>Full Stack Web Applications, 
-                        AI/ML Models, C++ Driver Development</span> and 
-                        more during these internships.
-                        <br />
-                        <br />
-                        I am very passionate toward having a significant impact while learning 
-                        and developing skills as much as possible.
-                        
-                    </Typography>
-                    <Grid item sx={{ mb: 4, mt: 5 }}>
-                        <TechStack />
-                    </Grid>
-                </Grid>
-                <Grid item xs={4} sx={{ mt: 1 }}>
-                    <DP />
-                </Grid>
+            <ThemeProvider theme={theme}>
+                <Grid container sx={{ mb: 2, mt: 1 }}>
+                    <Grid item xs={10}sx={{ mt: 1}}>
+                        <div className="mt-2 mb-2">
+                            <Typography variant="h4" align="left" color={textColour}>
+                                Hi, I'm
+                            </Typography>
+                            <Typography variant="h1" align="left" color={headingStyle}>
+                                Chinmay Pillai
+                            </Typography>
+                            <Typography variant="h3" align="left" color={headingStyle}>
+                                I'm a
+                                <span style={{fontWeight: "bold", color: "#06d6a0", marginInline: "10px"}}>
+                                    Developer
+                                </span>
+                                from
+                                <span style={{fontWeight: "bold", color: "#06d6a0", marginLeft: "10px"}}>
+                                    IIT Kanpur
+                                </span>
+                            </Typography>
+                        </div>
                 
-            </Grid>
+                    </Grid>
+                    <Grid item xs={2} sx={{ mt: 1 }}>
+                        <DP />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="h3" align="left" color={headingStyle} sx={{ mb: 2}}>
+                            I love
+                            <span style={{fontWeight: "bold", color: "#06d6a0", marginLeft: "10px"}}>
+                                {myPassions}
+                            </span>
+                            <span style={{color: "white"}}>
+                                <Cursor />
+                            </span>
+                        </Typography>
+                        <Typography variant="h6" align="left" color={textColour} >
+                            I am a curious developer studying in IIT Kanpur
+                            with <span style={{ fontWeight: 'bold' }}>1.5 years</span> of
+                            internship experience in software. I have worked
+                            on <span style={{ fontWeight: 'bold' }}>Full Stack Web Applications,
+                            AI/ML Models, C++ Driver Development</span> and
+                            more during these internships.
+                            <br />
+                            <br />
+                            I am very passionate toward having a significant impact while learning
+                            and developing skills as much as possible.
+                
+                        </Typography>
+                        <Grid item sx={{ mb: 4, mt: 5 }}>
+                            <TechStack />
+                        </Grid>
+                    </Grid>
+                
+                </Grid>
+            </ThemeProvider>
         </Container>
     );
 }
