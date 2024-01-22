26f6c6c8e6cd8a1b3869371cefec52e15685ee53
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-01-23 00:00:33 +0530

    Clean Navbar code

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -138,17 +138,6 @@ function NavBar() {
                         <NavItem item={Resume}/>
                     </div>
                 </ul>
-                {/*<form className="d-flex" role="search">
-                    <input
-                    className="form-control me-2"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
-                    />
-                    <button className="btn btn-outline-success" type="submit">
-                    Search
-                    </button>
-                </form>*/}
             </div>
             </div>
         </nav>
