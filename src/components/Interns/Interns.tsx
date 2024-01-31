279239d4144845ed4a2942577e0b26917ce6c212
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-01-31 22:03:07 +0530

    Organise Intern Images

--- src/components/Interns/Interns.tsx
+++ src/components/Interns/Interns.tsx
@@ -14,28 +14,28 @@ const InternList: Intern[] = [
     title: "SDE Summer Intern, Samsung Banglore",
     des: "Upcoming Summer SDE Intern for SRI, Banglore",
     date: "May-July 2024",
-    img: "/Samsung.jpg",
+    img: "/intern/Samsung.jpg",
     name: "Samsung"
   },
   {
     title: "SDE Summer Intern, ACL Digital",
     des: "Worked on IoT, MERN Stack, Computer Networks",
     date: "May-July 2023",
-    img: "/ACL.jpg",
+    img: "/intern/ACL.jpg",
     name: "ACL"
   },
   {
     title: "Computational Research Intern, IITK",
     des: "Worked on Neural Networks using PyTorch",
     date: "May 22 - May 23",
-    img: "/IITK.jpg",
+    img: "/intern/IITK.jpg",
     name: "IITK"
   },
   {
     title: "SDE Winter Intern, Quazar Technologies",
     des: "Worked on C++ Drivers, Gravity Simulator, Video Processing",
     date: "Oct-Dec 2022",
-    img: "/Quazar.jpg",
+    img: "/intern/Quazar.jpg",
     name: "Quazar"
   },
 ]
